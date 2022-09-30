9030864c4835c90be79466ba5542d62cc0131445
Jaiden Lee <jaidenwlee@gmail.com>
2022-09-30 16:26:08 -0400

    Idk what I did
    Copy and pasted some code lol to use Retrofit, which lets us make HTTP Requests

--- TestSendDataApplication/app/src/main/java/com/example/testsenddataapplication/MainActivity.kt
+++ TestSendDataApplication/app/src/main/java/com/example/testsenddataapplication/MainActivity.kt
@@ -2,10 +2,67 @@ package com.example.testsenddataapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+
+interface QuotesApi {
+    @GET("/quotes")
+    suspend fun getQuotes() : Response<QuoteList>
+}
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val quotesApi = RetrofitHelper.getInstance().create(QuotesApi::class.java)
+        GlobalScope.launch {
+            val result = quotesApi.getQuotes()
+            if (result!=null) {
+                Log.d("TEST", result.body().toString())
+            }
+        }
+
     }
 }
+
+object RetrofitHelper {
+
+    val baseUrl = "https://quotable.io/"
+
+    fun getInstance(): Retrofit {
+        return Retrofit.Builder().baseUrl(baseUrl)
+            .addConverterFactory(GsonConverterFactory.create())
+            // we need to add converter factory to
+            // convert JSON object to Java object
+            .build()
+    }
+}
+
+
+data class QuoteList(
+    val count: Int,
+    val lastItemIndex: Int,
+    val page: Int,
+    val results: List<Result>,
+    val totalCount: Int,
+    val totalPages: Int
+)
+
+data class Result(
+    val _id: String,
+    val author: String,
+    val authorSlug: String,
+    val content: String,
+    val dateAdded: String,
+    val dateModified: String,
+    val length: Int,
+    val tags: List<String>
+)
