d1364f2481408bbd9e412ac8d3b098fbae6b7e35
Jaiden Lee <jaidenwlee@gmail.com>
2022-10-01 09:14:44 -0400

    Made it so it prints the query parameters
    Gonna try and make it send requests, so that we can send images to serverside

--- test_server.js
+++ test_server.js
@@ -1,18 +1,29 @@
 const http = require("http") // importing the http library in the http variable
 const fs = require("fs") // file handling
+const url = require("url")
 const port = 3000
+const util = require("util")
 
+// localhost:3000/test/?name="bob"&id=1234
+// get request
 const server = http.createServer(function(req, res) {
-    res.writeHead(200, {"Content-Type": "text/html"})
-    fs.readFile("index.html", function(error, data) {
-        if (error) {
-            res.writeHead(404)
-            res.write('Error: File Not Found')
-        } else {
-            res.write(data)
-        }
-        res.end()
-    })
+    let path = url.parse(req.url, true)
+    res.writeHead(200, {"Content-Type":"text/plain"})
+    res.write(util.inspect(path.query)+"\n")
+    res.end();
+
+
+
+    // res.writeHead(200, {"Content-Type": "text/html"})
+    // fs.readFile("index.html", function(error, data) {
+    //     if (error) {
+    //         res.writeHead(404)
+    //         res.write('Error: File Not Found')
+    //     } else {
+    //         res.write(data)
+    //     }
+    //     res.end()
+    // })
 }) // every time someone requests osmethign on our server, it calls this function right here
 
 server.listen(port, function(error) {
